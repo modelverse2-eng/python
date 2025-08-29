e7f731d768d056069a8c5c2dd7db9fd55d5b6c17
modelverse2-eng <modelverse2@gmail.com>
2025-08-29 14:33:24 +0800

    Create leetcode第9题（18-1）.py

---
+++ leetcode第9题（18-1）.py
@@ -0,0 +1,16 @@
+给定一个整数x，如果x是a，则返回true回文，否则false。
+
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        # 负数或者末尾为0但不是0本身，直接不是回文
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+
+        reversed_half = 0
+        while x > reversed_half:
+            reversed_half = reversed_half * 10 + x % 10
+            x = x // 10
+
+        # 偶数位或奇数位回文
+        return x == reversed_half or x == reversed_half // 10
